be0642492153e2673232662859a2f6e532ce4d6e
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-20 17:19:45 +0100

    remove arg

--- includes/dbConnection.inc.php
+++ includes/dbConnection.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-function connect ($dns) {
+function connect () {
 	try {
 		$connection = new PDO('mysql:host=infodb2.iut.univ-metz.fr;dbname=dumont28u_trombi', 'dumont28u_appli', 'APPLIwepek7ombawpegyean', array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
