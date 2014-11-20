9b2cf41b18806000aa2f2dae85deaa3789db86e1
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-20 20:17:10 +0100

    peiruqgh

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -3,11 +3,13 @@ $items = array(
 	'trombi/index.php'=>'<a href="index.php" title="Voir tous les groupes"><span>Groupes</span></a>',
 	''=>'<a href="deconnection.php" title="Se déconnecter"><span>Déconnexion</span></a>',
 ); ?>
-<nav>
-  <ul>
+<header>
+  <nav>
+  	<ul>
 <?php
 foreach($items as $key => $item)
-	echo '	<li'.(($_SERVER['PHP_SELF']==$key)?' class="active"':'').'>'.$item.'</li>'."\n";
+	echo '	  <li'.(($_SERVER['PHP_SELF']==$key)?' class="active"':'').'>'.$item.'</li>'."\n";
 ?>
-  </ul>
-</nav>
+	</ul>
+  </nav>
+</header>
