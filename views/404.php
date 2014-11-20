22629dae270e345c3aac5a52717ce3d4165a2d07
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-20 18:13:55 +0100

    deleted menu

--- views/404.php
+++ views/404.php
@@ -1,9 +1,4 @@
 <!--meta title="Trombinoscope | Nooooooon Erreur 404 !"-->
-<nav>
-  <ul>
-	  <li><a href="deconnection.php" title="Se déconnecter">Déconnexion</a></li>
-  </ul>
-</nav>
 <div id="wrapper" style="text-align: center">
   <p style="font-size:404px; font-weight:bold; line-height:1; padding:0; margin:0;">404</p>
   <p>Ressource introuvable</p>
