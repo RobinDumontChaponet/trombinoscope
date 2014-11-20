9bad93b72a2ac0c68ae8b749711aaf546243d4b9
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-20 20:23:32 +0100

    something, don't hurt me robin

--- views/group.php
+++ views/group.php
@@ -5,5 +5,11 @@
 	foreach($group->getStudents() as $student)
 		echo '<figure><img src="data/group-0/student-0.jpg" alt="" /><figcaption>'.$student->getName().' '.$student->getFirstName().'</figcaption></figure>';
 ?>
+	<figure class="add">
+		<a href="?requ=ajoutEleve" title="Ajouter un élève">Ajouter</a>
+		<figcaption>
+			Ajouter un élève
+		</figcaption>
+	</figure>
   </section>
 </div>
