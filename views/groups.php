1ba5989b8edff14ea3517593235a8be107538b20
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-20 19:57:32 +0100

    correct IDs

--- views/groups.php
+++ views/groups.php
@@ -5,7 +5,7 @@
 			<li><a href="?requ=addgroup" title="Ajouter un groupe">Ajouter un groupe</a></li>
 			<?php
 				foreach ($groups as $group)
-					echo '<li>Groupe '.$group->getName().'<span class="date">('.$group->getDate().')</span><span><a href="?requ=group&id='.$group->getId.'" title="modifier">&#xe601;</a><a href="?requ=suppr&id='.$group->getId().'" title="supprimer">&#xe60d;</a></span></li>';
+					echo '<li>Groupe '.$group->getName().'<span class="date">('.$group->getDate().')</span><span><a href="?requ=group&id='.$group->getId().'" title="modifier">&#xe601;</a><a href="?requ=suppr&id='.$group->getId().'" title="supprimer">&#xe60d;</a></span></li>';
 			?>
 		</ol>
 	</section>
