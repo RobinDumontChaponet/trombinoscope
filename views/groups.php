9e4b2889f4ab49075d7667a31abfd28406bea073
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-20 18:00:49 +0100

    students

--- views/groups.php
+++ views/groups.php
@@ -5,7 +5,7 @@
 			<li><a href="?requ=addgroup" title="Ajouter un groupe">Ajouter un groupe</a></li>
 			<?php
 				foreach ($groups as $group)
-					echo '<li>Groupe '.$group->name.'<span class="date">('.$group->date.')</span><span><a href="?requ=group&id='.$group->idGroup.'" title="modifier">&#xe601;</a><a href="?requ=suppr&id='.$group->idGroup.'" title="supprimer">&#xe60d;</a></span></li>';
+					echo '<li>Groupe '.$group->name.'<span class="date">('.$group->date.')</span><span><a href="?requ=students&id='.$group->idGroup.'" title="modifier">&#xe601;</a><a href="?requ=suppr&id='.$group->idGroup.'" title="supprimer">&#xe60d;</a></span></li>';
 			?>
 		</ol>
 	</section>
---
+++ views/students.php
@@ -0,0 +1,17 @@
+<!--meta title="Trombinoscope | Groupe n°" css="style/grid.css"-->
+<div id="wrapper">
+  <section id="content">
+  	<figure>
+  	  <img src="data/group-0/student-0.jpg" alt="" />
+  	  <figcaption>Machin TRUC</figcaption>
+  	</figure>
+  	<figure>
+  	  <img src="data/group-0/student-0.jpg" alt="" />
+  	  <figcaption>Machin TRUC</figcaption>
+  	</figure>
+	<?php
+	//getStudentsById($_GET['id'];
+	echo $_GET['id'];
+?>
+  </section>
+</div>
