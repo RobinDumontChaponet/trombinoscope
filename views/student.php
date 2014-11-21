9a0d6c71fc8a16d85bcea5cdda8e52667837d92a
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-21 16:29:00 +0100

    blabla

--- views/student.php
+++ views/student.php
@@ -1,15 +1,13 @@
-<!--meta title="Trombinoscope | Groupe n°" css="style/grid.css"-->
+<!--meta title="Trombinoscope | Ajouter une image" css="style/grid.css"-->
 <div id="wrapper">
   <section id="content">
-<?php
-	foreach($group->getStudents() as $student)
-		echo '<figure><img src="data/group-0/student-0.jpg" alt="" /><figcaption>'.$student->getName().' '.$student->getFirstName().'</figcaption></figure>';
-?>
-	<figure class="add">
-		<a href="?requ=addStudent" title="Ajouter un élève">Ajouter</a>
-		<figcaption>
-			Ajouter un élève
-		</figcaption>
-	</figure>
+	<div>
+		Nom : <?php echo $_SESSION['trombiStudent']->getNom ?>
+	</div>
+	<div id="file">
+		<fieldset id="add" class="button">
+			<label>Sélectionnez une image à envoyer</label> <input type="file" id="fileinput" name="file"> <img src="style/images/loader.gif" alt="chargement...">
+		</fieldset>
+	</div>
   </section>
 </div>
