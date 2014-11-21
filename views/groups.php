3cb89cfb48af0c784d1ae0ed02c35e452d50056e
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-21 21:01:13 +0100

    style again + added (empty, for now) delete.js

--- views/groups.php
+++ views/groups.php
@@ -1,9 +1,9 @@
-<!--meta title="Trombinoscope | Groupes"-->
+<!--meta title="Trombinoscope | Groupes" js="script/delete.js"-->
 <section id="content">
   <ol>
   	<li><a href="?requ=addgroup" title="Ajouter un groupe">Ajouter un groupe</a></li>
 <?php foreach ($groups as $group)
-		  echo '<li>Groupe '.$group->getName().'<span class="date">('.$group->getDate().')</span><aside><a href="?requ=group&id='.$group->getId().'" title="modifier"><span>Modifier</span></a><a href="?requ=suppr&id='.$group->getId().'" title="supprimer"><span>Supprimer</span></a></aside></li>';
+		  echo '<li>Groupe '.$group->getName().'<span class="date">('.$group->getDate().')</span><aside><a href="?requ=group&id='.$group->getId().'" title="modifier"><span>Modifier</span></a><a href="?requ=suppr&id='.$group->getId().'" onclick="delete(this); return false" title="supprimer"><span>Supprimer</span></a></aside></li>';
 ?>
   </ol>
 </section>
