a63f1f67c4af1b0cb2f21a63b9cbe1ee4aeae8b4
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-25 17:44:31 +0100

    Réglages

--- controllers/preferences.php
+++ controllers/preferences.php
@@ -3,6 +3,9 @@
 	if($authId==0) {
 		include_once(MODELS_INC.'User.class.php');
 
+		$admin = getUserByLogin("admin");
+		$teacher = getUserByLogin("professeur");
+		
 		include(VIEWS_INC.'preferences.php');
 	} else
 		include(CONTROLLERS_INC.'403.php');
--- views/preferences.php
+++ views/preferences.php
@@ -1,4 +1,7 @@
-<!--meta title="Trombinoscope | Options admin" css="style/group.css" css="style/student.css"-->
+<!--meta title="Trombinoscope | Options admin"-->
 <section id="content" class="admin">
-   Hello
+
+<ol>
+	<?php echo '<li><form action="" method="post"><h1>Nom de compte : '.$admin->getLogin().'</h1><span>Mot de passe : <input type="password" name="pwdAdmin" value="" placeholder="Password"></span><div><input type="submit" value="Enregistrer" title="Enregistrer"></a></div></li><li><h1>Nom de compte : '.$teacher->getLogin().'</h1><span>Mot de passe : <input type="password" name="pwdTeacher" value="" placeholder="Password"></span><div><input type="submit" value="Enregistrer" title="Enregistrer"></a></div></form></li>';?>
+</ol>
 </section>
