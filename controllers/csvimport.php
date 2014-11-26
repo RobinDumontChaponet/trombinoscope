73081c5570cabd8f93474ea2f6a3ab4166200600
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-26 20:51:33 +0100

    csv, la suite

--- controllers/csvimport.php
+++ controllers/csvimport.php
@@ -1,7 +1,14 @@
 <?php
 
 if($_SESSION['trombiUser']->getAuth()->getId()==0) { // user is Admin
-	include(VIEWS_INC.'csvimport.php');
+
+	if(!empty($_POST)) {
+		foreach($_POST as $key => $value) {
+			echo 'POST parameter '.$key.' has '.$value;
+		}
+	} else
+		include(VIEWS_INC.'csvimport.php');
+
 } else
 	include(CONTROLLERS_INC.'403.php'); // user is NOT Admin
 
