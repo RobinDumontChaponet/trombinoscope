d55d9773364c81e67b41a093407450374d714b66
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-26 20:20:57 +0100

    students

--- views/students-teacher.php
+++ views/students-teacher.php
@@ -1,21 +1,16 @@
-<!--meta title="Trombinoscope | Groupe <?php echo $group->getName(); ?>" css="style/group.css"-->
+<!--meta title="Trombinoscope | Étudiants" css="style/groups.css"-->
 <section id="content">
-<?php
-echo '	<header><h1>Groupe '.$group->getName().'</h1>';
-if($group->getStartDate()!='' && $group->getEndDate()!='')
-	echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
-echo '</header>';
-
-if($authId==0) {
-?>
-  <figure class="add">
-	<a href="?requ=addStudent" title="Ajouter un élève">Ajouter</a>
-	<figcaption>Ajouter un élève</figcaption>
-  </figure>
-<?php
+  <ol>
+<?php $emptyGroup = new Group ('', 'Pas de groupe');
+if(count($students)>0)
+	foreach ($students as $student) {
+		$group=getGroupByStudent($student);
+		if(!$group) $group=$emptyGroup;
+		echo '<li>'.$student->getName().' '.$student->getFirstName().'<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
+		if($group->getStartDate()!='' && $group->getEndDate()!='')
+			echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
+		echo '</a></li>';
 }
-if(count($group->getStudents())>0)
-	foreach($group->getStudents() as $student)
-		echo '  <figure><img src="'.((is_file('data/images/thumbnails/student-'.$student->getId().'.jpg'))?'data/images/thumbnails/student-'.$student->getId().'.jpg':'style/images/nobody.png').'" alt="" /><figcaption>'.$student->getName().' '.$student->getFirstName().'</figcaption></figure>';
 ?>
+  </ol>
 </section>
