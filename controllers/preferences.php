43b4b4e43b68367316fa53ed385cfd8c8a78fade
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-26 22:22:32 +0100

    ROUAHHH

--- controllers/preferences.php
+++ controllers/preferences.php
@@ -5,7 +5,14 @@
 
 		$admin = getUserByLogin("admin");
 		$teacher = getUserByLogin("professeur");
-		
+
+		if(!empty($_POST['pwdTeacher'])) {
+			$teacher->setPwd($_POST['pwdTeacher']);
+		}
+		if(!empty($_POST['pwdAdmin'])) {
+			$admin->setPwd($_POST['pwdAdmin']);	// Faire une confirmation et crypter le mdp ?
+			//updateUser($admin);
+		}
 		include(VIEWS_INC.'preferences.php');
 	} else
 		include(CONTROLLERS_INC.'403.php');
