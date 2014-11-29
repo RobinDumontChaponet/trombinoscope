720596f46b3d46bf18d9e964500f8af863847483
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-29 17:01:58 +0100

    pdf

--- views/group-teacher.php
+++ views/group-teacher.php
@@ -6,14 +6,6 @@ if($group->getStartDate()!='' && $group->getEndDate()!='')
 	echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
 echo '</header>';
 
-if($authId==0) {
-?>
-  <figure class="add">
-	<a href="?requ=addStudent" title="Ajouter un élève">Ajouter</a>
-	<figcaption>Ajouter un élève</figcaption>
-  </figure>
-<?php
-}
 if(count($group->getStudents())>0)
 	foreach($group->getStudents() as $student)
 		echo '  <figure><img src="'.((is_file('data/images/thumbnails/student-'.$student->getId().'.jpg'))?'data/images/thumbnails/student-'.$student->getId().'.jpg':'style/images/nobody.png').'" alt="" /><figcaption>'.$student->getName().' '.$student->getFirstName().'</figcaption></figure>';
