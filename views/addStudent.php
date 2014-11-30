d4988693ad3b3de4d61ef4fb2091dfd542f9c331
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-30 17:46:34 +0100

    bla bla

--- views/addStudent.php
+++ views/addStudent.php
@@ -1,6 +1,8 @@
 <!--meta title="Trombinoscope | Selectionner des étudiants" css="style/groups.css" css="style/modal.css" js="script/delete.js"-->
 <section id="content">
 	<ol>
+		<form>
+		<li><input type="submit" value="Ajouter les étudiants séléctionnés"></li>
 		<li><a href="?requ=student" title="Ajouter un étudiant">Ajouter un étudiant</a></li>
 		<?php $emptyGroup = new Group ('', 'Pas de groupe');
 		if(count($students)>0)
@@ -10,9 +12,10 @@
 			echo '<li>'.$student->getName().' '.$student->getFirstName().' |<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
 			if($group->getStartDate()!='' && $group->getEndDate()!='')
 				echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
-			echo '</a><aside><form><input type="checkbox" name="students[]" value="Ajouter"></form></aside></li>';
+			echo '</a><aside><input type="checkbox" name="students[]" value="Ajouter"></aside></li>';
 		}
 		?>
+		<li><input type="submit" value="Ajouter les étudiants séléctionnés"><form></li>
 	</ol>
     <footer>
 		<p><?php
