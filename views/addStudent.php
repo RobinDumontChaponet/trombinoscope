2d021d1d288d327c5b9f12847de163217c63036a
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-30 17:37:59 +0100

    Yeahhhh

--- views/addStudent.php
+++ views/addStudent.php
@@ -1,16 +1,16 @@
-<!--meta title="Trombinoscope | Selectionner des étudiants" css="style/groups.css" css="style/modal.css" js="script/delete.js"-->
+<!--meta title="Trombinoscope | Selectionner des Ã©tudiants" css="style/groups.css" css="style/modal.css" js="script/delete.js"-->
 <section id="content">
 	<ol>
-		<li><a href="?requ=student" title="Ajouter un groupe">Ajouter un étudiant</a></li>
+		<li><a href="?requ=student" title="Ajouter un Ã©tudiant">Ajouter un Ã©tudiant</a></li>
 		<?php $emptyGroup = new Group ('', 'Pas de groupe');
 		if(count($students)>0)
 		foreach ($students as $student) {
 			$group=getGroupByStudent($student);
 			if(!$group) $group=$emptyGroup;
-			echo '<li>'.$student->getName().' '.$student->getFirstName().'<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
+			echo '<li>'.$student->getName().' '.$student->getFirstName().' |<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
 			if($group->getStartDate()!='' && $group->getEndDate()!='')
 				echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
-			echo '</a><aside><a href="index.php?requ=student&id='.$student->getId().'" title="modifier"><span>Modifier</span></a><a href="index.php?requ=student&suppr='.$student->getId().'" onclick="supprModal(this); return false" title="supprimer"><span>Supprimer</span></a></aside></li>';
+			echo '</a><aside><form><input type="checkbox" name="students[]" value="Ajouter"></form></aside></li>';
 		}
 		?>
 	</ol>
@@ -18,9 +18,9 @@
 		<p><?php
 			$stud = count($students);
 			if($stud>1)
-				echo $stud.' étudiants';
+				echo $stud.' Ã©tudiants';
 			else
-				echo $stud.' étudiant'
+				echo $stud.' Ã©tudiant'
 		?></p>
 	</footer>
 </section>
