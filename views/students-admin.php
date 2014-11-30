af596b1823312af72fbef342c7d44da73225a7cc
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-30 21:57:42 +0100

    + link

--- views/students-admin.php
+++ views/students-admin.php
@@ -7,7 +7,7 @@
 		foreach ($students as $student) {
 			$group=getGroupByStudent($student);
 			if(!$group) $group=$emptyGroup;
-			echo '<li>'.$student->getName().' '.$student->getFirstName().' |<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
+			echo '<li><a href="index.php?requ=student&id='.$student->getId().'" title="modifier">'.$student->getName().' '.$student->getFirstName().'</a> |<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
 			if($group->getStartDate()!='' && $group->getEndDate()!='')
 				echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
 			echo '</a><aside><a href="index.php?requ=student&id='.$student->getId().'" title="modifier"><span>Modifier</span></a><a href="index.php?requ=student&suppr='.$student->getId().'" onclick="supprModal(this); return false" title="supprimer"><span>Supprimer</span></a></aside></li>';
