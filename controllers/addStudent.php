018e5d763dac0a6c9d600c3d50aeed4e0d2d456e
JozwickiVictor <tigre.demon@hotmail.fr>
2014-11-30 19:48:36 +0100

    qzr

--- controllers/addStudent.php
+++ controllers/addStudent.php
@@ -2,11 +2,15 @@
 
 $authId=$_SESSION['trombiUser']->getAuth()->getId();
 if($authId == 0) {
+
 	include_once(MODELS_INC.'Group.class.php');
 
 	$students = getStudents();
 
 	include(VIEWS_INC.'addStudent.php');
+
+	$options = $_GET['options'];
+	var_dump($options);
 } else
 	include(CONTROLLERS_INC.'403.php');
 
--- views/addStudent.php
+++ views/addStudent.php
@@ -1,7 +1,7 @@
 <!--meta title="Trombinoscope | Selectionner des étudiants" css="style/groups.css" css="style/modal.css" js="script/delete.js"-->
 <section id="content">
 	<ol>
-		<form action="addStudent.php" method="post" name="addStudent">
+		<form action="?requ=addStudent&idGroup=<?php echo $_GET['idGroup']?>" method="post" name="addStudent">
 		<li><input type="submit" value="Ajouter les étudiants séléctionnés"></li>
 		<li><a href="?requ=student" title="Ajouter un étudiant">Ajouter un étudiant</a></li>
 		<?php $emptyGroup = new Group ('', 'Pas de groupe');
@@ -12,7 +12,7 @@
 			echo '<li>'.$student->getName().' '.$student->getFirstName().' |<a href="?requ=group&id='.$group->getId().'" title="Voir le groupe">Groupe '.$group->getName();
 			if($group->getStartDate()!='' && $group->getEndDate()!='')
 				echo '<span class="date">('.$group->getStartDate().'-'.$group->getEndDate().')</span>';
-			echo '</a><aside><input type="checkbox" name="students[]" value="Ajouter"></aside></li>';
+			echo '</a><aside><input type="checkbox" name="$options[]" value="'.$student->getId().'"></aside></li>';
 		}
 		?>
 		<li><input type="submit" value="Ajouter les étudiants séléctionnés"><form></li>
